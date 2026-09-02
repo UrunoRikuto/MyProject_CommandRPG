7b5f40e17904d66a0418a2625583d719cf9a2f69
UrunoRikuto <182198322+UrunoRikuto@users.noreply.github.com>
2026-09-02 17:05:50 +0900

    プロジェクトリセット

--- Assets/Script/SkillSO.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-// SkillDB用の ScriptableObject
-// Note: Skill 本体の定義は `Skill.cs` にある。
-[CreateAssetMenu(fileName = "New SkillDB", menuName = "ScriptableObjects/SkillDB")]
-public class SkillSO : ScriptableObject
-{
- public Skill[] skill;
-}
